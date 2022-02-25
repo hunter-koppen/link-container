edd559fd619a94818ccefecd448ac4ba5943b600
hunterkoppenmansystems <hunter.koppen@mansystems.nl>
2022-02-25 23:44:02 +0100

    finished widget

--- src/LinkContainer.jsx
+++ src/LinkContainer.jsx
@@ -1,7 +1,11 @@
 import { createElement } from "react";
 
+import "./ui/LinkContainer.css";
+
 export function LinkContainer({ url, content, target }) {
-    return <a className="link-container" href={url.value} target={target}>
-        <div className="link-container-content">{content}</div>
-    </a>;
+    return (
+        <a className="link-container" href={url.value} target={target}>
+            <div className="link-container-content">{content}</div>
+        </a>
+    );
 }
