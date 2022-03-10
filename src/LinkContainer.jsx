f5bce8e3a4ffe4b8ce42b6c66ec579b9b6470707
hunterkoppenmansystems <hunter.koppen@mansystems.nl>
2022-03-10 20:25:24 +0100

    Added stop propagation to the onclick

--- src/LinkContainer.jsx
+++ src/LinkContainer.jsx
@@ -3,8 +3,12 @@ import { createElement } from "react";
 import "./ui/LinkContainer.css";
 
 export function LinkContainer({ url, content, target }) {
+    const handleClick = e => {
+        e.stopPropagation();
+    };
+
     return (
-        <a className="link-container" href={url.value} target={target}>
+        <a className="link-container" href={url.value} target={target} onclick={handleClick}>
             <div className="link-container-content">{content}</div>
         </a>
     );
